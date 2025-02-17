18e328ee79de64e095055871704a6190e1f97343
258030 <258030@usn.no>
2025-02-17 18:49:34 +0100

    some design

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,11 @@
 import "../assets/styles/login.css"
+import pointer from "../assets/images/place.png"
 
 export function Login () {
 
     return (
     <div className="container">
+        <img src={pointer} alt="pointer" className="pointer"></img>
         <div className="containerBox">
             <div className="loginBox">
             <h2 id="login">LOGIN</h2>
@@ -14,11 +16,21 @@ export function Login () {
                     <input type="password" id="passwordInput" className="inputField" required/>
             </div>
             <div className="buttonGroup">
-                <button id="loginBtn" className="btn">LOGIN</button>
+                <button id="loginbtn" className="btn">LOGIN</button>
             </div>
             </div>
             <div className="divider">
-
+            <h2 id="info">Get more insight</h2>
+                <div className="infoBox">
+                        <p className="info-text">By being logged in you will be able to get more 
+                        relevant news based on your preferences, region and your own custom filters.
+                        It's totally free to make an account and use our services. 
+                        <br/><br/>Join us today!
+                        </p>
+                    </div>
+                <div className="buttonGroup">
+                    <button id="signbtn" className="btn">SIGNUP</button>
+                </div>
             </div>
         </div>
     </div>
